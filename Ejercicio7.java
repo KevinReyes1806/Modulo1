7c5da32eb9c7b05ee1c1c0a4d9987131c2e6233a
KevinReyes1806 <kvallecillo06@gmail.com>
2024-11-07 18:04:50 -0600

    Ejercicio7
    Escribe un programa que imprima mediante un ciclo los números pares del 2 al 100

---
+++ Ejercicio7.java
@@ -0,0 +1,15 @@
+package com.mycompany.ejercicio7;
+
+/**
+ *
+ * @author kvall
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+          for (int i = 2; i <= 100; i += 2)
+         {
+            System.out.println(i);
+         }
+    }
+}
