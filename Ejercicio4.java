6c05dbe7687737a5554c33451ee7aa6b98ef7a2f
KevinReyes1806 <kvallecillo06@gmail.com>
2024-11-07 18:01:45 -0600

    Ejercicio4
    Crear un arrelo que guarde e imprima 10 nombres de tus compañeros de la clase

---
+++ Ejercicio4.java
@@ -0,0 +1,24 @@
+package com.mycompany.ejercicio4;
+
+
+public class Ejercicio4 {  
+   public static void main(String[] args) {
+    
+    String[] nombres = new String [10];
+    nombres[0]= "Erick Ramon Cueva Pineda";
+    nombres[1]= "Gissel Anahi Acosta Perez";
+    nombres[2]= "Juan Carlos Cabrera Rodriguez";
+    nombres[3]= "Carlos Luis Reyes Bu";
+    nombres[4]= "Edgar Joel Guzman";
+    nombres[5]= "Eduardo yasir Gullen Rodriguez";
+    nombres[6]= "Jose Antonio Rodriguez";
+    nombres[7]= "Elvin Medez Matute";
+    nombres[8]= "Eduardo Jose Vallecillo Flores";
+    nombres[9]= "Emely Yesenia Reyes Vallecillo";
+    
+    for (int i=0; i <= 9; i++) 
+    {
+         System.out.println(nombres[i]);
+    }
+    }
+}
